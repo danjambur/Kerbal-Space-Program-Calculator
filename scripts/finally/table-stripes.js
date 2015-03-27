9f5a34619f83738b33f2623221b19e4db56c304e
danjambur <danny.j.burnett@gmail.com>
2015-03-27 18:43:39 +0000

    removed useless files

--- scripts/finally/table-stripes.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function()
-{ 
-	// Tiger-stripe tables			 
-    $('table tr').hover(
-        function() { $(this).addClass ('hover'); }, 
-        function() { $(this).removeClass ('hover'); }
-    );       
-    $("table tr:odd").addClass("oddrow");  
-});
